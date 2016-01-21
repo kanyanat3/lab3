a6a52a6326d7a9161aed492742532d62b1eabf21
Kanyanat Tongprasearth <ktongpra@ucsd.edu>
2016-01-21 21:22:37 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,45 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("Experience Chan!");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	//
+	$("#submitBtn").click(changeDetails());
 }
+
+function changeDetails(e){
+	var projectID = $('#project').val();
+	$(projectID).animate({
+		width: $('#width').val()
+	});
+	var addedText=$('#description').val();
+	$(projectID + ".project-description").text(addedText);
+
+}
+
+function projectClick(e){
+	e.preventDefault();
+	
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+	console.log("Target matched: " + jumbotronHeader.length);
+
+	var containingProject = $(this).closest(".project");
+	var description= $(containingProject).find(".project-description");
+	if(description.length ==0){
+		containingProject.append("<div class='project-description'><p>>Description of the project.</p></div>");
+	}else{
+		$(description).fadeOut();
+	}
+}
+
+
+
+
+
